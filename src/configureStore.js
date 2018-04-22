bc823e1c2f1c4b970ef3e996332104117c9c76a1
tusharc88 <tushar54983@gmail.com>
2018-04-22 14:02:38 -0700

    Wrapping store.dispatch function in rootReducers.js to log incoming actions, prev state and next state

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,8 +1,24 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import throttle from "lodash/throttle";
-import rootReducer from "./reducers/rootReducer";
-import { loadState, saveState } from "./localStorage";
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import throttle from 'lodash/throttle';
+import rootReducer from './reducers/rootReducer';
+import { loadState, saveState } from './localStorage';
+
+const addLoggingToDispatch = store => {
+  const rawDispatch = store.dispatch;
+  if (!console.group) return rawDispatch;
+
+  return action => {
+    console.group(action.type);
+    console.log('%c prev state: ', 'color: gray', store.getState());
+    console.log('%c action: ', 'color: blue', action);
+    const returnValue = rawDispatch(action);
+    console.log('%c next state: ', 'color: green', store.getState());
+    console.groupEnd(action.type);
+
+    return returnValue;
+  };
+};
 
 const configureStore = () => {
   // const persistedState = loadState();
@@ -19,6 +35,9 @@ const configureStore = () => {
     )
   );
 
+  if (process.env.NODE_ENV !== 'production') {
+    store.dispatch = addLoggingToDispatch(store);
+  }
   // store.subscribe(
   //   throttle(() => {
   //     saveState({
